ec7306273760562f6be73e82c8f858b38ffe1eba
Tabyretka <opqwerty20@yandex.ru>
2023-04-29 13:25:10 +0000

    Tiny edit

--- data/states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-
-class AniStates(StatesGroup):
-    name = State()
-    chooseRes = State()
-    episode = State()
-    player = State()
